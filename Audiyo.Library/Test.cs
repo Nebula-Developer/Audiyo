6de1538b7b49a9e98921741cf5b0f1451cc4edc2
NebulaDev <nebuladev.contact@gmail.com>
2023-06-25 18:54:31 +1200

    Initial Commit

---
+++ Audiyo.Library/Test.cs
@@ -0,0 +1,11 @@
+﻿using ManagedBass;
+
+namespace Audiyo.Library;
+
+public class Class1 {
+    public static void Test() {
+        Bass.Init();
+        Console.WriteLine(Bass.LastError);
+        Bass.Free();
+    }
+}
